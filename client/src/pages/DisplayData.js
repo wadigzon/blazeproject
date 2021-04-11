b36b2fd729f1456c6277f453211ebb4af5c0c8a7
WADIGZON DIAZ-WONG <wadigzon@users.noreply.github.com>
2021-04-11 03:10:44 -0700

    ag-grid working and loading data

--- client/src/pages/DisplayData.js
+++ client/src/pages/DisplayData.js
@@ -4,19 +4,22 @@ import LoadingComponent from '../components/LoadingComponent'
 import { toastError } from "../utils/toastrFunctions";
 import { BASE_URL_CUSTOMERS } from "../constants/urls";
 import { getRequest } from "../utils/urlFunctions";
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 export default class DisplayData extends Component {
     constructor(props) {
         super();
         this.state = {
             isLoading: true,
-            data: undefined
+            rowData: undefined
         };
     }
     componentDidMount() {
 
         const callback = (data) => {
-            this.setState({ isLoading: false, data: data });
+            this.setState({ isLoading: false, rowData: data });
             console.log(data);
         }
         const errorCallback = (body) => {
@@ -49,13 +52,25 @@ export default class DisplayData extends Component {
     }
 
     render() {
-        const { isLoading  } = this.state
+        const { isLoading, rowData  } = this.state
 
         return isLoading ?
         (<LoadingComponent isLoading />) :
         (
-            <div className='displaydata'>
+            <div>
+                <div style={{paddingLeft: "10px"}}>
                 <h1>Display Data</h1>
+                </div>
+
+                <div className="ag-theme-alpine" style={{ height: 400, width: '80%', paddingLeft:'10px', paddingTop:'10px' }}>
+                    <AgGridReact
+                        rowData={rowData}>
+                        <AgGridColumn field="firstName" sortable={ true } filter={ true }></AgGridColumn>
+                        <AgGridColumn field="lastName" sortable={ true } filter={ true }></AgGridColumn>
+                        <AgGridColumn field="email" sortable={ true } filter={ true }></AgGridColumn>
+                        <AgGridColumn field="phoneNumber" sortable={ true } filter={ true }></AgGridColumn>
+                    </AgGridReact>
+                </div>
             </div>
         )
     }
